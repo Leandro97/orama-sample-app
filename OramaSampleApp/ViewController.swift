6b0c84fd88d9f51ba64baeca008e8cf15ccf03b5
Leandro97 <leandro97.martins@gmail.com>
2021-04-13 12:49:24 -0300

    Setting up the project

---
+++ OramaSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OramaSampleApp
+//
+//  Created by Leandro Martins de Freitas on 13/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
